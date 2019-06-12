717baa1a77d5a860643cc164960997f2c668a924
Bob <4109153+LedLoaf@users.noreply.github.com>
2019-06-12 00:46:27 -0700

    Create Math.h

---
+++ Maths/Math.h
@@ -0,0 +1,11 @@
+#ifndef MATH_HPP
+#define MATH_HPP
+
+#include "Distribution.hpp"
+#include "Generics.hpp"
+#include "Random.hpp"
+#include "Trigonometry.hpp"
+#include "Vector2.hpp"
+#include "Vector3.hpp"
+
+#endif
